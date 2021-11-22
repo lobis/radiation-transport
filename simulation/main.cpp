556b25a4125c82a505ccf5507003567b2ae3f549
Luis Obis <luis.antonio.obis@gmail.com>
2021-11-22 13:48:25 +0100

    correctly linked local lib

--- simulation/main.cpp
+++ simulation/main.cpp
@@ -2,12 +2,17 @@
 // Created by lobis on 22/11/2021.
 //
 
-// #include <TestClass.h>
-
 #include <spdlog/spdlog.h>
 #include <yaml-cpp/yaml.h>
 
+#include "TestClass.h"
+
 int main() {
     spdlog::info("This is the simulation application!");
+
+    TestClass test;
+
+    spdlog::info(test.i);
+
     return 0;
 }
