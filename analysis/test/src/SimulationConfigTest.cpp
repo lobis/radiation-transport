353f984be59badc8c333c7b3f93e2afeb5adc927
Luis Obis <luis.antonio.obis@gmail.com>
2021-11-23 00:54:34 +0100

    improved relative paths

--- analysis/test/src/SimulationConfigTest.cpp
+++ analysis/test/src/SimulationConfigTest.cpp
@@ -4,10 +4,16 @@
 
 #include <gtest/gtest.h>
 
+#include <filesystem>
+
 #include "SimulationConfig.h"
 
+using namespace std;
+
+auto testsBasePath = string(std::filesystem::path(__FILE__).parent_path().parent_path()) + "/";
+
 TEST(SimulationConfig, LoadFromFile) {
-    const auto config = SimulationConfig::LoadFromFile("files/simulation.yaml");
+    const auto config = SimulationConfig::LoadFromFile(testsBasePath + "files/simulation.yaml");
 
     EXPECT_EQ(config.fSerialMode, "serial");
 }
