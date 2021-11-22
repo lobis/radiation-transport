c2fd435dd82f787f7035afb2d233d6763aba42c4
lobis <luis.antonio.obis@gmail.com>
2021-11-22 19:25:57 +0100

    Minor changes to cmake

--- simulation/main.cpp
+++ simulation/main.cpp
@@ -5,14 +5,20 @@
 #include <spdlog/spdlog.h>
 #include <yaml-cpp/yaml.h>
 
-#include "TestClass.h"
+#include <G4RunManagerFactory.hh>
+#include <G4UIExecutive.hh>
+#include <G4UImanager.hh>
+#include <G4VisExecutive.hh>
+
+#include "SimulationConfig.h"
 
 int main() {
+    spdlog::set_level(spdlog::level::info);
+    spdlog::set_pattern("[%T][%^%l%$][thread %t]: %v");
+
     spdlog::info("This is the simulation application!");
 
-    TestClass test;
-
-    spdlog::info(test.i);
+    auto config = SimulationConfig::LoadFromFile("../analysis/test/files/simulation.yaml");
 
     return 0;
 }
