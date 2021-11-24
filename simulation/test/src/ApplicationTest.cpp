bef75b3e55470ae3f6fd1dad87e5e757e3b23b4e
Luis Obis <luis.antonio.obis@gmail.com>
2021-11-24 12:25:36 +0100

    Defined simulation testing structure

---
+++ simulation/test/src/ApplicationTest.cpp
@@ -0,0 +1,23 @@
+//
+// Created by lobis on 11/22/2021.
+//
+
+#include <gtest/gtest.h>
+
+#include <filesystem>
+
+#include "Application.h"
+
+using namespace std;
+
+auto examplesPath = string(std::filesystem::path(__FILE__).parent_path().parent_path().parent_path()) + "/examples/";
+
+TEST(ApplicationTest, LoadFromFile) {
+    const string exampleConfig = testsBasePath + "basic/simulation.yaml";
+
+    Application app;
+
+    app.LoadConfigFromFile(exampleConfig);
+
+    app.Run();
+}
