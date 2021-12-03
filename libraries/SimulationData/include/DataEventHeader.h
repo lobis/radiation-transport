4169106fef2ff91e3cf168e51b0d46ba31b10adc
Luis Obis <luis.antonio.obis@gmail.com>
2021-12-03 23:41:49 +0100

    placed SimulationGeometryInfo in separate header for dict generation

--- libraries/SimulationData/include/DataEventHeader.h
+++ libraries/SimulationData/include/DataEventHeader.h
@@ -11,37 +11,7 @@
 #include <map>
 #include <vector>
 
-class G4VPhysicalVolume;
-
-class SimulationGeometryInfo {
-    ClassDefInline(SimulationGeometryInfo, 1);
-
-   public:
-    std::vector<TString> fGdmlNewPhysicalNames;
-    std::map<TString, TString> fGeant4PhysicalNameToNewPhysicalNameMap; /*
-                                                                         * only makes sense when using assembly
-                                                                         */
-    std::vector<TString> fPhysicalVolumes;                              /* Geant4 physical names */
-    std::vector<TString> fLogicalVolumes;
-    std::vector<TString> fMaterials;
-
-    std::map<TString, TString> fPhysicalToLogicalVolumeMap;
-    std::map<TString, std::vector<TString> > fLogicalToPhysicalMap;
-    // many physical volumes can point to one single logical
-    std::map<TString, TString> fLogicalToMaterialMap;
-    std::map<TString, TVector3> fPhysicalToPositionInWorldMap;
-
-   public:
-    inline SimulationGeometryInfo() = default;
-
-    void PopulateFromGdml(const TString&);
-
-    TString GetAlternativeNameFromGeant4PhysicalName(const TString&) const;
-
-    Int_t GetIDFromVolumeName(const TString&) const;
-
-    void PopulateFromGeant4World(const G4VPhysicalVolume*);
-};
+#include "SimulationGeometryInfo.h"
 
 class DataEventHeader {
     ClassDef(DataEventHeader, 1);
