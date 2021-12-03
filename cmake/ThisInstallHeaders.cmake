d8c6704e8cd6a7a8077f906d9edcf266a9af4d7e
Luis Obis <luis.antonio.obis@gmail.com>
2021-12-03 20:42:55 +0100

    added install of examples

--- cmake/ThisInstallHeaders.cmake
+++ cmake/ThisInstallHeaders.cmake
@@ -1,4 +1,4 @@
 macro(THIS_INSTALL_HEADERS)
     FILE(GLOB HEADERS "include/*.h")
-    install(FILES ${HEADERS} DESTINATION ${CMAKE_INSTALL_PREFIX}/headers)
+    install(FILES ${HEADERS} DESTINATION ${CMAKE_INSTALL_PREFIX}/include)
 endmacro()
