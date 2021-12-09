f430eed2b4bef27aabbfc2b54cbd9e150cc07e60
Luis Obis <luis.antonio.obis@gmail.com>
2021-12-09 01:21:57 +0100

    now dictionaries work on install

--- cmake/ThisInstallHeaders.cmake
+++ cmake/ThisInstallHeaders.cmake
@@ -1,4 +1,6 @@
 macro(THIS_INSTALL_HEADERS)
     FILE(GLOB HEADERS "include/*.h")
     install(FILES ${HEADERS} DESTINATION ${CMAKE_INSTALL_PREFIX}/include)
+
+    include_directories(${CMAKE_INSTALL_PREFIX}/include)
 endmacro()
