59298d6bf53298b8306bb93fb19547d93f813190
lobis <luis.antonio.obis@gmail.com>
2022-03-03 14:04:29 +0100

    fixed and updated veto example

--- applications/simulation/examples/veto/validate.C
+++ applications/simulation/examples/veto/validate.C
@@ -19,14 +19,43 @@ int validate(const TString& filename = "exampleSingleVetoMuonsFromDisk.root") {
     cout << "Event Tree has " << EventTree->GetEntries() << " events" << endl;
 
     double averageEnergy = 0;
+    double minEnergy = 0;
+    double maxEnergy = 0;
+
+    double averagePrimaryEnergy = 0;
+    double minPrimaryEnergy = 0;
+    double maxPrimaryEnergy = 0;
+
     for (int i = 0; i < EventTree->GetEntries(); i++) {
         EventTree->GetEntry(i);
-        averageEnergy += fEvent->fSensitiveVolumesTotalEnergy / EventTree->GetEntries();
+        auto energy = fEvent->fSensitiveVolumesTotalEnergy;
+        averageEnergy += energy / EventTree->GetEntries();
+
+        if (energy < minEnergy || i == 0) {
+            minEnergy = energy;
+        }
+        if (energy > maxEnergy) {
+            maxEnergy = energy;
+        }
+
+        auto primaryEnergy = fEvent->fPrimaryEnergy;
+        averagePrimaryEnergy += primaryEnergy / EventTree->GetEntries();
+
+        if (primaryEnergy < minPrimaryEnergy || i == 0) {
+            minPrimaryEnergy = primaryEnergy;
+        }
+        if (primaryEnergy > maxPrimaryEnergy) {
+            maxPrimaryEnergy = primaryEnergy;
+        }
     }
 
-    cout << "Average energy is " << averageEnergy << " keV" << endl;
+    cout << "Average sensitive energy is " << averageEnergy << " keV" << endl;
+    cout << "Min / Max sensitive energy is " << minEnergy << " / " << maxEnergy << " keV" << endl;
 
-    if (averageEnergy <= 13500 && averageEnergy > 12500) {
+    cout << "Average primary energy is " << averagePrimaryEnergy << " keV" << endl;
+    cout << "Min / Max primary energy is " << minPrimaryEnergy << " / " << maxPrimaryEnergy << " keV" << endl;
+
+    if (averageEnergy <= 12500 && averageEnergy > 10500) {
         cout << "Average energy within range" << endl;
         return 0;
     }
